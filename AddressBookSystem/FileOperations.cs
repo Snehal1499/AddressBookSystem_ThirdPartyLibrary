fe61c05d9ea99b7b52090880b2a7115c8f9bdd7e
Snehal1499 <snehalpatil1718@gmail.com>
2022-04-25 23:29:22 +0530

    Added UC13 Read Write Address Book Problem

--- AddressBookSystem/FileOperations.cs
+++ AddressBookSystem/FileOperations.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace AddressBookSystem
 {
-    internal class FileOperations
+    public class FileOperations
     {
+        public void FileExits()
+        {
+            string path = @"D:\.Net program\AddressBookSystem_ThirdPartyLibrary\AddressBookSystem\AddressBook.txt";
+            if (File.Exists(path))
+            {
+                Console.WriteLine("File Exists");
+            }
+            else
+            {
+                Console.WriteLine("File Not Found");
+            }
+
+        }
+
+
     }
 }
